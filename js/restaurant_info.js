9836d03f5b0d3e404ef1da0a36dfdd60640ec40c
ayamfouad <aya@duot.io>
2018-03-28 14:09:03 +0200

    fixes

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -177,7 +177,12 @@ createReviewHTML = (review) => {
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
-  const li = document.createElement('li');
+	const breadcrumbElements = breadcrumb.querySelectorAll('li');
+	for (element of breadcrumbElements) {
+	element.removeAttribute('aria-current');
+}
+	const li = document.createElement('li');
+	li.setAttribute('aria-current', 'page');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
 }
